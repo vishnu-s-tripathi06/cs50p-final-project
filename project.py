887bbc6424e96145361e06be241f5e5f5f683780
Vishnu Shankar Tripathi <vstripathi400@gmail.com>
2026-03-31 23:20:21 +0530

    Start

---
+++ project.py
@@ -0,0 +1,18 @@
+def main():
+    print("Welcome to Finance CLI")
+
+
+def add_transaction():
+    pass
+
+
+def calculate_balance():
+    pass
+
+
+def list_transactions():
+    pass
+
+
+if __name__ == "__main__":
+    main()  
