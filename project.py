73c9547562449989e31a1a2abe4c308f932d3ea2
unknown <vstripathi400@gmail.com>
2026-04-08 17:41:16 +0530

    Project begin

--- project.py
+++ project.py
@@ -1,5 +1,14 @@
 def main():
     print("Welcome to Finance CLI")
+    Option_number=0
+    print("1: Add transaction")
+    print("2: View transaction")
+    print("3: Show balance")
+    print("4: Exit")
+    while(Option_number!=4):
+
+        Option_number=int(input("Enter a choice by typing 1 , 2 , 3 , 4 :"))
+        
 
 
 def add_transaction():
